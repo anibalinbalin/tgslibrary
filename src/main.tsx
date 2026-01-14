d9943794ad22f4e6beecef60e85910ee8857f418
michelletliu <michelletliu@g.ucla.edu>
2026-01-13 17:40:20 -0800

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/globals.css";
@@ -10,6 +11,7 @@ console.log("✨ thanks for stopping by! say hi: michelletheresaliu@gmail.com");
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>
 );
   
